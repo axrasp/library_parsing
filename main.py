1f1975df83c71c32d7d38fb1242d3c6e6d7477af
axrasp <aaraspopin@gmail.com>
2022-04-27 13:09:28 +0300

    add parse comments

--- main.py
+++ main.py
@@ -1,19 +1,83 @@
-import requests
+import os
 from pathlib import Path
+from urllib.parse import unquote, urljoin, urlsplit
+
+import requests
+from bs4 import BeautifulSoup
+from pathvalidate import sanitize_filename
 
 
-def main():
-    bookfolder = 'books'
+def check_for_redirect(response):
+    if response.history:
+        raise requests.HTTPError()
+
+
+def get_book():
+    bookfolder = 'books/'
     Path(bookfolder).mkdir(parents=True, exist_ok=True)
-    for book_id in range(1, 10):
-        url = f'https://tululu.org/txt.php?id={book_id}'
+    for book_id in range(7, 8):
+        url = f'https://tululu.org/b{book_id}/'
         print(url)
         response = requests.get(url)
         response.raise_for_status()
-        filename = f"book{book_id}.txt"
-        with open(f'{bookfolder}/{filename}', 'wb') as file:
-            file.write(response.content)
+        print(response.status_code)
+        try:
+            check_for_redirect(response)
+            book = get_book_info(url=url)
+            filename = f'{book_id}. {book["title"]}'
+            #download_txt(url=url, filename=filename, folder='books/')
+            download_image(url=book['image_url'], folder='images/')
+        except Exception as exc:
+            print(exc)
+
+
+def get_book_info(url):
+    book_comments = {}
+    response = requests.get(url)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text, 'lxml')
+    book_name_parsed = soup.find('h1').text.split(' \xa0 :: \xa0 ')
+    book_image_path = soup.find(class_='bookimage').find('img')['src']
+    book_comments_parsed = soup.find_all(class_='texts')
+    for comment in book_comments_parsed:
+        name = comment.find('b').text
+        comment = comment.find(class_='black').text
+        book_comments[name] = comment
+    print(book_comments)
+    book_image_url = urljoin(url, book_image_path)
+    book = dict(zip(['title', 'author'], book_name_parsed))
+    book['image_url'] = book_image_url
+    book['comments'] = book_comments
+    return book
+
+
+def download_txt(url: str, filename: str, folder: str):
+    Path(folder).mkdir(parents=True, exist_ok=True)
+    response = requests.get(url)
+    response.raise_for_status()
+    filename = sanitize_filename(filename)
+    file_path = os.path.join(folder, filename)
+    with open(f'{file_path}.txt', 'wb') as file:
+        file.write(response.content)
+    with open(f'{file_path}.txt', 'rb') as file:
+        book_text = file.read()
+        return book_text
+
+
+def download_image(url: str, folder: str):
+    Path(folder).mkdir(parents=True, exist_ok=True)
+    response = requests.get(url)
+    response.raise_for_status()
+    image_path = unquote(urlsplit(url).path)
+    image_name = os.path.basename(image_path)
+    print(image_name)
+    with open(f'{folder}/{image_name}', 'wb') as file:
+        file.write(response.content)
+
+
+def main():
+    get_book()
 
 
 if __name__ == '__main__':
-    main ()
+    main()
