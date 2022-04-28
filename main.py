f610f62221960679240ee44efab5bcdc311b782c
axrasp <aaraspopin@gmail.com>
2022-04-28 14:08:33 +0300

    connectionError/http exception added, code refactored

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import re
 from pathlib import Path
 from urllib.parse import unquote, urljoin, urlsplit
+import time
 
 import requests
 from bs4 import BeautifulSoup
@@ -30,26 +31,41 @@ def get_book(start_id: str, end_id: str):
     Path(bookfolder).mkdir(parents=True, exist_ok=True)
     for book_id in range(start_id, end_id):
         url = f'https://tululu.org/b{book_id}/'
-        response = requests.get(url)
-        response.raise_for_status()
+        for i in range(3):
+            try:
+                response = requests.get(url)
+                response.raise_for_status()
+                break
+            except requests.exceptions.ConnectionError as e:
+                print(e)
+                print(f"Не удалось скачать книгу {book_id}. "
+                      f"Ошибка подключения. "
+                      f"Повторное подключение к серверу {3-i}")
+                time.sleep(5)
+            except requests.exceptions.HTTPError as e:
+                print(e)
+                print(f"Не удалось скачать книгу {book_id}. Книга не найдена")
+                break
         try:
             check_for_redirect(response)
-            book = parse_book_page(url=url)
+            book = parse_book_page(html=response.text, url=url)
             print(f"Название: {book['title']}")
             print(f"Автор: {book['author']}")
             filename = f'{book_id}. {book["title"]}'
             download_txt(url=url, filename=filename, folder='books/')
             download_image(url=book['image_url'], folder='images/')
 
-        except Exception as exc:
-            print(exc)
+        except requests.exceptions.HTTPError as e:
+            print(e)
+            print(f"Не удалось скачать книгу {book_id}. Книга не найдена")
+        except requests.exceptions.ConnectionError as e:
+            print(e)
+            print("Не удалось подключиться к серверу")
 
 
-def parse_book_page(url):
+def parse_book_page(html, url):
     book_comments = {}
-    response = requests.get(url)
-    response.raise_for_status()
-    soup = BeautifulSoup(response.text, 'lxml')
+    soup = BeautifulSoup(html, 'lxml')
     book_name_parsed = soup.find('h1').text.split(' \xa0 :: \xa0 ')
     book_image_path = soup.find(class_='bookimage').find('img')['src']
     book_comments_parsed = soup.find_all(class_='texts')
@@ -87,6 +103,7 @@ def download_image(url: str, folder: str):
     image_name = os.path.basename(image_path)
     with open(f'{folder}/{image_name}', 'wb') as file:
         file.write(response.content)
+
 
 def main():
     parser = create_parser()
